5c7071085025e089513f2c14a7ea4b9cb4c4d90b
Yael.Tzirulnikov <yael@redislabs.com>
2019-03-13 11:38:29 +0200

    Implement tsbs_load_redistimeseries

---
+++ cmd/tsbs_load_redistimeseries/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"github.com/timescale/tsbs/load"
+	"log"
+)
+
+// Program option vars:
+var (
+	host        string
+)
+
+// Global vars
+var (
+	loader *load.BenchmarkRunner
+	//bufPool sync.Pool
+)
+// allows for testing
+var fatal = log.Fatal
+
+// Parse args:
+func init() {
+	loader = load.GetBenchmarkRunnerWithBatchSize(1000)
+	flag.StringVar(&host, "host", "localhost:6379", "Provide host:port for redis connection")
+	flag.Parse()
+}
+
+type benchmark struct {
+	dbc *dbCreator
+}
+
+func (b *benchmark) GetPointDecoder(br *bufio.Reader) load.PointDecoder {
+	return &decoder{scanner: bufio.NewScanner(br)}
+}
+
+func (b *benchmark) GetBatchFactory() load.BatchFactory {
+	return &factory{}
+}
+
+func (b *benchmark) GetPointIndexer(maxPartitions uint) load.PointIndexer {
+	return &load.ConstantIndexer{}
+}
+
+func (b *benchmark) GetProcessor() load.Processor {
+	return &processor{b.dbc}
+}
+
+func (b *benchmark) GetDBCreator() load.DBCreator {
+	return b.dbc
+}
+type processor struct {
+	dbc *dbCreator
+}
+
+func (p *processor) Init(_ int, _ bool) {}
+
+// ProcessBatch reads eventsBatches which contain rows of data for TS.ADD redis command string
+func (p *processor) ProcessBatch(b load.Batch, doLoad bool) (uint64, uint64) {
+	events := b.(*eventsBatch)
+	if doLoad {
+		conn := p.dbc.client.Pool.Get()
+		for _, row := range events.rows {
+			sendRedisCommand(row, conn)
+		}
+		conn.Flush()
+
+	}
+	rowCnt := uint64(len(events.rows))
+	metricCnt := rowCnt
+	events.rows = events.rows[:0]
+	ePool.Put(events)
+	return metricCnt, rowCnt
+}
+
+
+func main() {
+	loader.RunBenchmark(&benchmark{dbc: &dbCreator{}}, load.SingleQueue)
+}
---
+++ cmd/tsbs_load_redistimeseries/scan.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"strings"
+	"sync"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/timescale/tsbs/load"
+)
+
+type decoder struct {
+	scanner *bufio.Scanner
+}
+
+// Reads and returns a text line that encodes a data point for a specif field name.
+// Since scanning happens in a single thread, we hold off on transforming it
+// to an INSERT statement until it's being processed concurrently by a worker.
+func (d *decoder) Decode(_ *bufio.Reader) *load.Point {
+	ok := d.scanner.Scan()
+	if !ok && d.scanner.Err() == nil { // nothing scanned & no error = EOF
+		return nil
+	} else if !ok {
+		log.Fatalf("scan error: %v", d.scanner.Err())
+	}
+	return load.NewPoint(d.scanner.Text())
+}
+
+func sendRedisCommand(line string, conn redis.Conn) {
+	t := strings.Split(line, " ")
+	s := make([]interface{}, len(t))
+	for i, v := range t {
+		s[i] = v
+	}
+	err := conn.Send("TS.ADD", s...)
+	if err != nil {
+		fatal("TS.ADD failed: %s\n", err)
+	}
+}
+type eventsBatch struct {
+	rows []string
+}
+
+func (eb *eventsBatch) Len() int {
+	return len(eb.rows)
+}
+
+func (eb *eventsBatch) Append(item *load.Point) {
+	that := item.Data.(string)
+	eb.rows = append(eb.rows, that)
+}
+
+var ePool = &sync.Pool{New: func() interface{} { return &eventsBatch{rows: []string{}} }}
+
+type factory struct{}
+
+func (f *factory) New() load.Batch {
+	return ePool.Get().(*eventsBatch)
+}

ab0f13eeaf111f353e6d204d37a473a8015d7ea2
Danni Moiseyev <danni@redislabs.com>
2019-03-17 09:48:31 +0200

    Shard data over workers

--- cmd/tsbs_load_redistimeseries/main.go
+++ cmd/tsbs_load_redistimeseries/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"flag"
 	"github.com/timescale/tsbs/load"
+	"io"
 	"log"
+	"strings"
+	"crypto/md5"
 )
 
 // Program option vars:
@@ -19,6 +23,7 @@ var (
 )
 // allows for testing
 var fatal = log.Fatal
+var md5h = md5.New()
 
 // Parse args:
 func init() {
@@ -31,6 +36,19 @@ type benchmark struct {
 	dbc *dbCreator
 }
 
+type RedisIndexer struct{
+	partitions uint
+}
+
+func (i *RedisIndexer) GetIndex(p *load.Point) int {
+	row := p.Data.(string)
+	key := strings.Split(row, " ")[0]
+	_, _ = io.WriteString(md5h, key)
+	hash := binary.LittleEndian.Uint32(md5h.Sum(nil))
+	md5h.Reset()
+	return int(uint(hash) % i.partitions)
+}
+
 func (b *benchmark) GetPointDecoder(br *bufio.Reader) load.PointDecoder {
 	return &decoder{scanner: bufio.NewScanner(br)}
 }
@@ -40,7 +58,7 @@ func (b *benchmark) GetBatchFactory() load.BatchFactory {
 }
 
 func (b *benchmark) GetPointIndexer(maxPartitions uint) load.PointIndexer {
-	return &load.ConstantIndexer{}
+	return &RedisIndexer{partitions: maxPartitions}
 }
 
 func (b *benchmark) GetProcessor() load.Processor {
@@ -58,16 +76,20 @@ func (p *processor) Init(_ int, _ bool) {}
 
 // ProcessBatch reads eventsBatches which contain rows of data for TS.ADD redis command string
 func (p *processor) ProcessBatch(b load.Batch, doLoad bool) (uint64, uint64) {
-	var err error
 	events := b.(*eventsBatch)
+	cmdLen := 0
 	if doLoad {
 		conn := p.dbc.client.Pool.Get()
 		for _, row := range events.rows {
 			sendRedisCommand(row, conn)
+			cmdLen++
 		}
-		_, err = conn.Do("")
-		if err != nil {
-			log.Fatalf("Error while inserting: %v", err)
+
+		for i := 0; i < cmdLen; i++ {
+			_, err := conn.Receive()
+			if err != nil {
+				log.Fatalf("Error while inserting: %v", err)
+			}
 		}
 	}
 	rowCnt := uint64(len(events.rows))

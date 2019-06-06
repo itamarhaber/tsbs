cbe801e35b3177843b04be0ec53f75c7f26ab088
Itamar Haber <itamar@redislabs.com>
2019-06-06 18:42:36 +0300

    Skips reply in pipeline

--- cmd/tsbs_load_redistimeseries/main.go
+++ cmd/tsbs_load_redistimeseries/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
 	"encoding/binary"
 	"flag"
-	"github.com/timescale/tsbs/load"
+	"fmt"
 	"io"
 	"log"
 	"strings"
-	"crypto/md5"
+
+	"github.com/gomodule/redigo/redis"
+
+	"github.com/timescale/tsbs/load"
 )
 
 // Program option vars:
 var (
-	host        string
+	host string
 )
 
 // Global vars
@@ -21,6 +25,7 @@ var (
 	loader *load.BenchmarkRunner
 	//bufPool sync.Pool
 )
+
 // allows for testing
 var fatal = log.Fatal
 var md5h = md5.New()
@@ -36,16 +41,16 @@ type benchmark struct {
 	dbc *dbCreator
 }
 
-type RedisIndexer struct{
+type RedisIndexer struct {
 	partitions uint
 }
 
 func (i *RedisIndexer) GetIndex(p *load.Point) int {
 	row := p.Data.(string)
 	key := strings.Split(row, " ")[0]
-	start := strings.Index(key,"{")
-	end := strings.Index(key,"}")
-	_, _ = io.WriteString(md5h,  key[start+1:end])
+	start := strings.Index(key, "{")
+	end := strings.Index(key, "}")
+	_, _ = io.WriteString(md5h, key[start+1:end])
 	hash := binary.LittleEndian.Uint32(md5h.Sum(nil))
 	md5h.Reset()
 	return int(uint(hash) % i.partitions)
@@ -70,6 +75,7 @@ func (b *benchmark) GetProcessor() load.Processor {
 func (b *benchmark) GetDBCreator() load.DBCreator {
 	return b.dbc
 }
+
 type processor struct {
 	dbc *dbCreator
 }
@@ -82,29 +88,26 @@ func (p *processor) ProcessBatch(b load.Batch, doLoad bool) (uint64, uint64) {
 	cmdLen := 0
 	if doLoad {
 		conn := p.dbc.client.Pool.Get()
-		sent := []string{}
+		conn.Send("CLIENT", "REPLY", "OFF")
 		for _, row := range events.rows {
-			cmds := strings.Split(row,";")
+			cmds := strings.Split(row, ";")
 			for i := range cmds {
 				if strings.TrimSpace(cmds[i]) == "" {
 					continue
 				}
-				sent = append(sent, cmds[i])
 				sendRedisCommand(cmds[i], conn)
 				cmdLen++
 			}
 		}
-
+		conn.Send("CLIENT", "REPLY", "ON")
 		err := conn.Flush()
 		if err != nil {
 			log.Fatalf("Error while inserting: %v", err)
 		}
 
-		for i := 0; i < cmdLen; i++ {
-			_, err = conn.Receive()
-			if err != nil {
-				log.Fatalf("Error while inserting: %v, cmd: '%s'", err,sent[i])
-			}
+		_, err = conn.Receive()
+		if err != nil {
+			log.Fatalf("Error while inserting: %v", err)
 		}
 	}
 	rowCnt := uint64(len(events.rows))
@@ -113,7 +116,38 @@ func (p *processor) ProcessBatch(b load.Batch, doLoad bool) (uint64, uint64) {
 	return uint64(cmdLen), rowCnt
 }
 
+func verifyRun() {
+	conn, err := redis.DialURL(fmt.Sprintf("redis://%s", host))
+	if err != nil {
+		log.Fatalf("Error while dialing %v", err)
+	}
+	_, err = conn.Do("PING")
+	if err != nil {
+		log.Fatalf("Error while PING %v", err)
+	}
+
+	cursor := 0
+	total := 0
+	for {
+		rep, _ := redis.Values(conn.Do("SCAN", cursor))
+		cursor, _ = redis.Int(rep[0], nil)
+		keys, _ := redis.Strings(rep[1], nil)
+		for _, key := range keys {
+			total++
+			info, _ := redis.Values(conn.Do("TS.INFO", key))
+			chunks, _ := redis.Int(info[5], nil)
+			if chunks != 30 {
+				log.Fatalf("Verification error: key %v has %v chunks\n", key, chunks)
+			}
+		}
+		if cursor == 0 {
+			break
+		}
+	}
+	log.Printf("Verified %v keys\n", total)
+}
 
 func main() {
 	loader.RunBenchmark(&benchmark{dbc: &dbCreator{}}, load.WorkerPerQueue)
+	verifyRun()
 }
